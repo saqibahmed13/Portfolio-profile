4752942e5bf2704965d20f362666a44c26703479
saqibahmed13 <saqiba0484@gmail.com>
2024-08-06 19:20:35 +0530

    added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Front End Developer", "Full Stack Developer", "UI/UX Designer" ];
   const period = 2000;
 
   useEffect(() => {
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Saqib`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Front End Developer", "Web Designer", "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>Hello, I’m Md Saqib Ahmed, a dedicated web developer with two years of experience specializing in frontend and backend development. I have a strong command of HTML5, CSS3 (including frameworks like Bootstarp, Tailwind CSS), JavaScript (ES6+), and frameworks such as React.js and Node.js. My expertise extends to crafting responsive, user-friendly websites and implementing backend solutions using MongoDB and Express JS. I thrive on collaborating with teams to deliver high-quality projects that meet both technical requirements and design expectations. Let's connect and explore how I can contribute to your next digital initiative.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
